a5c649b75047d0232e5fa5183088d64ff2a9e1f2
giraffedev <root@giraffedev.com>
2015-02-14 19:44:29 +0200

    Added spanish translations

---
+++ languages/es.php
@@ -0,0 +1,20 @@
+<?php
+
+/* File containing the english language strings translations */
+
+$strings["es"] = [
+	'hello' => 'Hola',
+	'bye' => 'Adios',
+	'welcome' => 'Bienvenida',
+	'sign in' => 'Iniciar la sesion',
+	'log out' => 'Finalizar la sesion',
+	'sign up' => 'Registrar',
+	'username' => 'Nombre',
+	'password' => 'Contrasena',
+	'home' => 'A casa',
+	'contact' => 'Contactar',
+	'message' => 'Mensaje',
+	'about us' => 'Sobre nosotros',
+	'Back' => 'Detras',
+	'Close' => 'Cerra'
+];
