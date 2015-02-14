4067134d88d406330e3d2dce283234f941d9b7b5
giraffedev <root@giraffedev.com>
2015-02-14 17:37:14 +0200

    Update en.strings.php
    Added first strings for english strings list.

--- languages/en.strings.php
+++ languages/en.strings.php
@@ -1,3 +1,14 @@
 <?php
 
 /* File containing the english language strings translations */
+
+$string["en"] = [
+	'hello' => 'Hello',
+	'bye' => 'Good bye',
+	'welcome' => 'Welcome',
+	'sign in' => 'Sign in',
+	'log out' => 'Log out',
+	'sign up' => 'Register',
+	'username' => 'Username',
+	'password' => 'Password'
+];
