6035e9745676816d10da1e4d1907335794994486
giraffedev <root@giraffedev.com>
2015-02-14 18:38:48 +0200

    Update project-string.class.php

--- project-string.class.php
+++ project-string.class.php
@@ -1,3 +1,34 @@
-<?php
+<?php 
 
-/* All magic will be placed in here */
+namespace Language;
+
+/**
+ *  Loading all strings translations
+ *  @return $strings[language][name]
+ */
+include 'languages/en.php';
+include 'languages/ro.php';
+
+class String {
+	public static $language;
+
+    /**
+     * [Sets the required string language]
+     * @param  string $language default: en
+     * @return [string]  
+     */
+	public static function lang($language = "en") {
+		self::$language = $language;
+		return new self();
+	}
+
+	/**
+	 * [Returns the string in the language set]
+	 * @param  string $name
+	 * @return [string]
+	 */
+	public function get($name = "") {
+		global $strings;
+		return $strings[self::$language][$name];
+	}
+}
