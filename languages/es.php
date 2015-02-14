4c102731f4b15f28abf1bc91e29da5c0f5c3cabd
giraffedev <root@giraffedev.com>
2015-02-14 20:02:46 +0200

    Commented out the spanish translations

--- languages/es.php
+++ languages/es.php
@@ -1,20 +1,25 @@
 <?php
 
-/* File containing the english language strings translations */
+/* File containing the spanish language strings translations */
 
 $strings["es"] = [
+	/* Greetings */
 	'hello' => 'Hola',
 	'bye' => 'Adios',
 	'welcome' => 'Bienvenida',
+	
+	/* User Interface */
 	'sign in' => 'Iniciar la sesion',
 	'log out' => 'Finalizar la sesion',
 	'sign up' => 'Registrar',
 	'username' => 'Nombre',
 	'password' => 'Contrasena',
+	'message' => 'Mensaje',
+	'back' => 'Detras',
+	'close' => 'Cerra',
+	
+	/* Page names */
 	'home' => 'A casa',
 	'contact' => 'Contactar',
-	'message' => 'Mensaje',
-	'about us' => 'Sobre nosotros',
-	'Back' => 'Detras',
-	'Close' => 'Cerra'
+	'about us' => 'Sobre nosotros'
 ];
