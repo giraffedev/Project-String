b106835d9a878248782b6a5d8da57faa1d4a24a2
giraffedev <root@giraffedev.com>
2015-02-14 17:38:59 +0200

    Update ro.strings.php
    Basica romanian language strings

--- languages/ro.strings.php
+++ languages/ro.strings.php
@@ -1,3 +1,14 @@
 <?php
 
 /* File containing the romanian language strings translations */
+
+$string["ro"] = [
+	'hello' => 'Buna',
+	'bye' => 'La revedere',
+	'welcome' => 'Bine ati venit',
+	'sign in' => 'Logati-va',
+	'log out' => 'Delogati-va',
+	'sign up' => 'Inregistrare',
+	'username' => 'Utilizator',
+	'password' => 'Parola'
+];
