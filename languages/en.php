f7c1bd63bf6816a4b1d5f32871ac3e2f6caeb40f
giraffedev <root@giraffedev.com>
2015-02-14 20:05:41 +0200

    Commented out the english translations

--- languages/en.php
+++ languages/en.php
@@ -3,18 +3,23 @@
 /* File containing the english language strings translations */
 
 $strings["en"] = [
+	/* Greetings */
 	'hello' => 'Hello',
 	'bye' => 'Good bye',
 	'welcome' => 'Welcome',
+	
+	/* User Interface */
 	'sign in' => 'Sign in',
 	'log out' => 'Log out',
 	'sign up' => 'Register',
 	'username' => 'Username',
 	'password' => 'Password',
+	'back' => 'Back',
+	'close' => 'Close',
+	'message' => 'Message',
+	
+	/* Page names */
 	'home' => 'Home',
 	'contact' => 'Contact',
-	'message' => 'Message',
-	'about us' => 'About us',
-	'Back' => 'Back',
-	'Close' => 'Close'
+	'about us' => 'About us'
 ];
