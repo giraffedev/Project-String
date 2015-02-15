5d777d6a406cff1a9cd90d3b6bf1ef81eb959d8d
giraffedev <root@giraffedev.com>
2015-02-15 12:56:48 +0200

    Added croatian translations

---
+++ languages/hr.php
@@ -0,0 +1,39 @@
+<?php
+
+/* File containing the croatian language strings translations */
+
+$strings["hr"] = [
+	/* Greetings */
+	'hello' => 'Zdravo',
+	'bye' => 'Doviðenja',
+	'welcome' => 'Dobrodošao',
+	
+	/* User Interface */
+	'sign in' => 'Prijava',
+	'log out' => 'Odjava',
+	'sign up' => 'Pridruži se',
+	'username' => 'Korisnièko ime',
+	'password' => 'Lozinka',
+	'back' => 'Nazad',
+	'close' => 'Zatvori',
+	'message' => 'Poruka',
+	'notification' => 'Obavijest',
+	'help' => 'Pomoæ',
+	'next' => 'Iduæa',
+	'previous' => 'Prijašnja',
+	'ok' => 'U redu',
+	'now' => 'Sad',
+	
+	/* Time related */
+	'year' => 'Godina',
+	'month' => 'Mjesec',
+	'day' => 'Dan',
+	'hour' => 'Sat',
+	'minute' => 'Minuta',
+	'second' => 'Sekunda',
+	
+	/* Page names */
+	'home' => 'Poèetna',
+	'contact' => 'Kontakt',
+	'about us' => 'O nama'
+];
