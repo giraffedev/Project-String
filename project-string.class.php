4abb194790060339647a1db1c80f90b01e8ac942
giraffedev <root@giraffedev.com>
2015-02-15 13:02:54 +0200

    Proper identation

--- project-string.class.php
+++ project-string.class.php
@@ -1,32 +1,33 @@
 <?php 
-
 namespace Language;
 
 /**
- *  Loading all strings translations
- *  @return $strings[language][name]
- */
+*  Loading all strings translations
+*  @return $strings[language][name]
+*/
 include 'languages/en.php';
 include 'languages/ro.php';
 include 'languages/es.php';
 include 'languages/hr.php';
 
 class String {
+
 	public static $language;
 
 	/**
-	 * [Sets the language of the page]
-	 * @param [string] $language default: en
-	 */
+	* [Sets the language of the page]
+	* @param [string] $language default: en
+	*/
+	
 	public static function set($language = "en") {
 		self::$language = $language;
 	}
 
-    /**
-     * [Sets the required string language]
-     * @param  string $language default: en
-     * @return [string]  
-     */
+	/**
+	* [Sets the required string language]
+	* @param  string $language default: en
+	* @return [string]  
+	*/
 	public static function lang($language = "en") {
 		self::$language = $language;
 		return new self();
@@ -41,7 +42,7 @@ class String {
 		global $strings;
 		return $strings[self::$language][$name];
 	}
-
+	
 	/**
 	 * [Calling: get as static method]
 	 * @return [string]
