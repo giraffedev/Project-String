ee3d0dd6243e90c613e864b07c49642c97e168ec
giraffedev <root@giraffedev.com>
2015-02-15 16:22:32 +0200

    Added danish language

---
+++ languages/dk.php
@@ -0,0 +1,39 @@
+<?php
+ 
+/* File containing the danish language strings translations */
+ 
+$strings["dk"] = [
+        /* Velkommen */
+        'hello' => 'Hej',
+        'bye' => 'Farvel',
+        'welcome' => 'Velkommen',
+       
+        /* User Interface */
+        'sign in' => 'Log ind',
+        'log out' => 'Log ud',
+        'sign up' => 'Registrer',
+        'username' => 'Brugernavn',
+        'password' => 'Adgangskode',
+        'back' => 'Tilbage',
+        'close' => 'Luk',
+        'message' => 'Besked',
+        'notification' => 'Underretning',
+        'help' => 'Hjælp',
+        'next' => 'Næste',
+        'previous' => 'Tilbage',
+        'ok' => 'Ok',
+        'now' => 'Nu',
+       
+        /* Time related */
+        'year' => 'År',
+        'month' => 'Måned',
+        'day' => 'Dag',
+        'hour' => 'Time',
+        'minute' => 'Minut',
+        'second' => 'Sekund',
+       
+        /* Page names */
+        'home' => 'Hjem',
+        'contact' => 'Kontakt',
+        'about us' => 'Omkring os'
+];
