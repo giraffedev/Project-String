a9ac5d6e546654b543d0cf304c183e83c1386979
giraffedev <root@giraffedev.com>
2015-02-15 16:11:31 +0200

    Added icelandic translations thanks to Amy

---
+++ languages/is.php
@@ -0,0 +1,39 @@
+<?php
+
+/* File containing the icelandic language strings translations */
+
+$strings["is"] = [
+	/* Greetings */
+	'hello' => 'Halló',
+	'bye' => 'Bless / Bæ',
+	'welcome' => 'Velkomin',
+	
+	/* User Interface */
+	'sign in' => 'Skrá mig inn',
+	'log out' => 'Skrá mig út',
+	'sign up' => 'Skrá mig á síðuna',
+	'username' => 'Notanda Nafn',
+	'password' => 'Lykilorð',
+	'back' => 'Til baka',
+	'close' => 'Loka',
+	'message' => 'Skilaboð',
+	'notification' => 'Fréttir',
+	'help' => 'Hjálp',
+	'next' => 'Næst',
+	'previous' => 'Til Baka',
+	'ok' => 'Allt í lægi',
+	'now' => 'Núna',
+	
+	/* Time related */
+	'year' => 'Ár',
+	'month' => 'Mánuður',
+	'day' => 'Dagur',
+	'hour' => 'Klukkutími',
+	'minute' => 'Minotur',
+	'second' => 'Secondur',
+	
+	/* Page names */
+	'home' => 'Heim',
+	'contact' => 'Hafa Samband',
+	'about us' => 'Um Okkur'
+];
