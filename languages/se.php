108c64176704a1c54016450ddf5dd130a1613662
giraffedev <root@giraffedev.com>
2015-02-15 16:21:20 +0200

    Added swedish language translations

---
+++ languages/se.php
@@ -0,0 +1,39 @@
+<?php
+
+/* File containing the swedish language strings translations */
+
+$strings["se"] = [
+	/* Greetings */
+	'hello' => 'Hej',
+	'bye' => 'Hej då',
+	'welcome' => 'Välkommen',
+
+	/* User Interface */
+	'sign in' => 'Logga in',
+	'log out' => 'Logga ut',
+	'sign up' => 'Registrera',
+	'username' => 'Användarnamn',
+	'password' => 'Lösenord',
+	'back' => 'Gå tillbaka',
+	'close' => 'Stäng',
+	'message' => 'Meddelande',
+	'notification' => 'Notifikation',
+	'help' => 'Hjälp',
+	'next' => 'Nästa',
+	'previous' => 'Föregående',
+	'ok' => 'Okej',
+	'now' => 'Nu',
+
+	/* Time related */
+	'year' => 'År',
+	'month' => 'Månad',
+	'day' => 'Dag',
+	'hour' => 'Timme',
+	'minute' => 'Minut',
+	'second' => 'Sekund',
+
+	/* Page names */
+	'home' => 'Hem',
+	'contact' => 'Kontakt',
+	'about us' => 'Om oss'
+];
